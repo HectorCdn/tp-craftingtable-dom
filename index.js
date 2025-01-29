cf502a90519bbd980a9a28fd0418a307595f50f3
HectorCdn <hcottachaudon@edenschool.fr>
2025-01-29 12:01:14 +0100

    qzdqdqzdqzd

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const MATERIAL_IRON_INGOT_ID = "IRON-INGOT";
 const MATERIAL_STICK_ID = "STICK";
 const MATERIAL_AIR_ID = "";
+const MATERIAL_BOW_ID = "BOW";
+const MATERIAL_DIAMOND_ID = "DIAMOND";
+const MATERIAL_STRING_ID = "STRING";
+const MATERIAL_OAKLOG_ID = "OAKLOG";
+const MATERIAL_OAKPLANKS_ID = "OAKPLANKS";
 
 const MATERIAL_AIR_IMG = "./assets/air.webp";
 const MATERIAL_IRON_INGOT_IMG = "./assets/iron-ingot.webp";
 const MATERIAL_STICK_IMG = "./assets/stick.webp";
+const MATERIAL_BOW_IMG = "./assets/bow.webp";
+const MATERIAL_DIAMOND_IMG = "./assets/diamond.webp";
+const MATERIAL_STRING_IMG = "./assets/string.webp";
+const MATERIAL_OAKLOG_IMG = "./assets/oaklog.webp";
+const MATERIAL_OAKPLANKS_IMG = "./assets/oakplanks.webp";
 
-const materials = [MATERIAL_AIR_ID, MATERIAL_IRON_INGOT_ID, MATERIAL_STICK_ID];
+const materials = [
+	MATERIAL_AIR_ID,
+	MATERIAL_IRON_INGOT_ID,
+	MATERIAL_STICK_ID,
+	MATERIAL_DIAMOND_ID,
+	MATERIAL_STRING_ID,
+	MATERIAL_OAKLOG_ID,
+	MATERIAL_OAKPLANKS_ID,
+];
+
 const materialsImageSrc = [
 	MATERIAL_AIR_IMG,
 	MATERIAL_IRON_INGOT_IMG,
 	MATERIAL_STICK_IMG,
+	MATERIAL_DIAMOND_IMG,
+	MATERIAL_STRING_IMG,
+	MATERIAL_OAKLOG_IMG,
+	MATERIAL_OAKPLANKS_IMG,
 ];
 
 const pickaxeRecipe = [
@@ -42,7 +65,7 @@ const recipeList = [pickaxeRecipe, axeRecipe];
 const recipeImageSrcList = [pickaxeRecipeImageSrc, axeRecipeImageSrc];
 
 const craftingTable = ["", "", "", "", "", "", "", "", ""];
-const inventoryTable = [0, 1, 2];
+const inventoryTable = [0, 1, 2, 3, 4, 5, 6];
 
 const result = document.getElementById("result-slot");
 
@@ -96,33 +119,4 @@ window.addEventListener("click", (e) => {});
 
 main.addEventListener("click", () => {
 	selectedButton.classList.remove("selected");
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
