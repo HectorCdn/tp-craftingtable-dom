77970e7f086140564c6e837ccbedc9bcae1c9e6b
HectorCdn <hcottachaudon@edenschool.fr>
2025-01-31 15:04:27 +0100

    sefjuiqehgqegjk

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const MATERIAL_DIAMOND_ID = "DIAMOND";
 const MATERIAL_STRING_ID = "STRING";
 const MATERIAL_OAKLOG_ID = "OAKLOG";
 const MATERIAL_OAKPLANKS_ID = "OAKPLANKS";
+const MATERIAL_ENCHANTED_BOOKS_ID = "ENCHANTED_BOOK";
+const MATERIAL_DIAMOND_PICKAXE_ID = "DIAMOND_PICKAXE";
+// const MATERIAL_ENCHANTED_DIAMOND_PICKAXE_ID = "ENCHANTED_DIAMOND_PICKAXE"
 
 const MATERIAL_AIR_IMG = "./assets/air.webp";
 const MATERIAL_IRON_INGOT_IMG = "./assets/iron-ingot.webp";
@@ -15,6 +18,9 @@ const MATERIAL_DIAMOND_IMG = "./assets/diamond.webp";
 const MATERIAL_STRING_IMG = "./assets/string.webp";
 const MATERIAL_OAKLOG_IMG = "./assets/oaklog.webp";
 const MATERIAL_OAKPLANKS_IMG = "./assets/oakplanks.webp";
+const MATERIAL_ENCHANTED_BOOKS_IMG = "./assets/enchanted_book.webp";
+const MATERIAL_DIAMOND_PICKAXE_IMG = "./assets/diamond_pickaxewebp.webp"
+// const MATERIAL_ENCHANTED_DIAMOND_PICKAXE_IMG = "./assets/enchanted_diamond_pickaxe.webp"
 
 const materials = [
 	MATERIAL_AIR_ID,
@@ -24,6 +30,8 @@ const materials = [
 	MATERIAL_STRING_ID,
 	MATERIAL_OAKLOG_ID,
 	MATERIAL_OAKPLANKS_ID,
+	MATERIAL_ENCHANTED_BOOKS_ID,
+	MATERIAL_DIAMOND_PICKAXE_ID,
 ];
 
 const materialsImageSrc = [
@@ -34,6 +42,8 @@ const materialsImageSrc = [
 	MATERIAL_STRING_IMG,
 	MATERIAL_OAKLOG_IMG,
 	MATERIAL_OAKPLANKS_IMG,
+	MATERIAL_ENCHANTED_BOOKS_IMG,
+	MATERIAL_DIAMOND_PICKAXE_IMG,
 ];
 
 const pickaxeRecipe = [
@@ -58,23 +68,120 @@ const axeRecipe = [
 	MATERIAL_STICK_ID,
 	MATERIAL_AIR_ID,
 ];
+const stickRecipe = [
+	MATERIAL_AIR_ID,
+	MATERIAL_OAKPLANKS_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_OAKPLANKS_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+];
+const oakplanksRecipe = [
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_OAKLOG_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+];
+const enchantedDiamondPickaxeRecipe = [
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_DIAMOND_PICKAXE_ID,
+	MATERIAL_ENCHANTED_BOOKS_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+];
 
+// const ??? = [
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// ];
+// const ??? = [
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// ];
+// const ??? = [
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// ];
+// const ??? = [
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// 	MATERIAL_AIR_ID,
+// ];
+
+
+const oakplanksRecipeImageSrc = "./assets/oakplanks.webp";
 const pickaxeRecipeImageSrc = "./assets/iron-pickaxe.webp";
 const axeRecipeImageSrc = "./assets/iron-axe.webp";
-const recipeList = [pickaxeRecipe, axeRecipe];
-const recipeImageSrcList = [pickaxeRecipeImageSrc, axeRecipeImageSrc];
+const stickRecipeImageSrc = "./assets/stick.webp";
+const enchantedDiamondPickaxeImageSrc = "./assets/enchanted_diamond_pickaxe.webp"
+
+const recipeList = [
+	pickaxeRecipe,
+	axeRecipe, 
+	stickRecipe,
+	oakplanksRecipe,
+	enchantedDiamondPickaxeRecipe,
+];
+
+const recipeImageSrcList = [
+	pickaxeRecipeImageSrc,
+	axeRecipeImageSrc,
+	stickRecipeImageSrc,
+	oakplanksRecipeImageSrc,
+	enchantedDiamondPickaxeImageSrc,
+];
 
 const craftingTable = ["", "", "", "", "", "", "", "", ""];
-const inventoryTable = [0, 1, 2, 3, 4, 5, 6];
+const inventoryTable = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 const result = document.getElementById("result-slot");
 
-const craft = document.querySelector(".crafting-slot");
+const craft = document.querySelectorAll(".crafting-slot");
 const inventoryElement = document.querySelector(".inventory-container");
+const imgCaseResult = document.querySelector("#imgCaseResult");
 
-let selectedMaterialName = "";
 let selectedMaterialImgSrc = "";
 let selectedButton = null;
+let pipette = "";
 
 function createButtonItem(imageSrc, imageAlt) {
 	const ButtonElement = document.createElement("button");
@@ -100,23 +207,63 @@ for (let i = 0; i < inventoryTable.length; i++) {
 	const newButton = createButtonItem(materialsImgSrc, materialName);
 
 	newButton.addEventListener("click", (e) => {
-		if (selectedButton === newButton) {
+		if (pipette === materialName && selectedButton === newButton) {
+			pipette = "";
+			selectedMaterialImgSrc = "";
 			selectedButton.classList.remove("selected");
-			selectedButton = null;
 		} else {
-			if (selectedButton) {
+			if (selectedButton !== null) {
 				selectedButton.classList.remove("selected");
 			}
+			pipette = materialName;
+			selectedMaterialImgSrc = materialsImgSrc;
 
 			selectedButton = newButton;
 			selectedButton.classList.add("selected");
 		}
+		console.log(pipette, verify(pickaxeRecipe, craftingTable), selectedButton);
 	});
 	inventoryElement.appendChild(newButton);
 }
 
-window.addEventListener("click", (e) => {});
+function verify(table1, table2) {
+	for (let i = 0; i < table1.length; i++) {
+		if (table1[i] !== table2[i]) {
+			return false;
+		}
+	}
 
-main.addEventListener("click", () => {
-	selectedButton.classList.remove("selected");
-});
+	return true;
+}
+
+for (let i = 0; i < craft.length; i++) {
+	const craftSlot = craft[i];
+
+	craftSlot.addEventListener("click", () => {
+		const newImage = craftSlot.firstElementChild;
+
+		if (newImage == null) return;
+
+		newImage.src = selectedMaterialImgSrc;
+		newImage.alt = pipette;
+
+		craftingTable[i] = pipette;
+
+		console.log(craftingTable, pickaxeRecipe);
+
+		for (let i = 0; i < recipeList.length; i++) {
+			const isValid = verify(recipeList[i], craftingTable);
+			if (isValid != false) {
+				imgCaseResult.src = recipeImageSrcList[i];
+				imgCaseResult.alt = "outil";
+				break;
+			} else {
+				imgCaseResult.src = MATERIAL_AIR_IMG;
+				imgCaseResult.alt = MATERIAL_AIR_ID;
+			}
+		}
+
+		console.log(craftSlot);
+		console.log(verify(pickaxeRecipe, craftingTable));
+	});
+}
